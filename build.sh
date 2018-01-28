9991a63587afe550eb5bcc271caa0aeadef825e2
Pubudu Ranasinghe <pubudutr@gmail.com>
2018-01-29 00:21:25 +0530

    sfksdjf

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 source ./build/envsetup.sh
+cd ~/android/lineage/vendor/wingtech
+git clone https://github.com/TheMuppets/proprietary_vendor_wingtech.git
+cd ~/android/lineage
 breakfast wt88047
 export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
 croot
